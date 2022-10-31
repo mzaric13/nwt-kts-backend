a900bccc64450ceee2259c6301f44466ab11b3f5
Matija Zarić <matijazaric9@gmail.com>
2022-10-31 16:01:17 +0100

    Initialized spring project 2.0

---
+++ src/main/java/nwt/kts/backend/NwtKtsBackendApplication.java
@@ -0,0 +1,13 @@
+package nwt.kts.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwtKtsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NwtKtsBackendApplication.class, args);
+    }
+
+}
--- src/test/java/nwt/kts/backend/NwtKtsBackendApplicationTests.java
+++ src/test/java/nwt/kts/backend/NwtKtsBackendApplicationTests.java
@@ -1,13 +1,13 @@
-package nwt.kts.project;
+package nwt.kts.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class NwtKtsBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
