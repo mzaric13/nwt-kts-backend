f8cf59af7f2293e4fbd6eed3667e1722bdaf61ad
Teodor Sakal Franciskovic <sakal.teodor@gmail.com>
2022-11-11 17:01:46 +0100

    Added temporary Route class

--- src/main/java/nwt/kts/backend/entity/Route.java
+++ src/main/java/nwt/kts/backend/entity/Route.java
@@ -1,5 +1,40 @@
 package nwt.kts.backend.entity;
 
+import javax.persistence.*;
+import java.awt.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "routes")
 public class Route {
-    // TODO
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+    @Column(name="route_name")
+    private String routeName;
+
+    @Column(name="start_point")
+    private Point start;
+
+    @Column(name="end_point")
+    private Point endPoint;
+
+    @Column(name = "expected_time", nullable = false)
+    private double expectedTime;
+
+    @Column(name = "length", nullable = false)
+    private double length;
+
+    @ElementCollection
+    @CollectionTable(name = "route_points", joinColumns = @JoinColumn(name = "id"))
+    @Column(name = "route_path", nullable = false)
+    private Set<Point> routePath;
+
+    public Route() {
+
+    }
+
 }
