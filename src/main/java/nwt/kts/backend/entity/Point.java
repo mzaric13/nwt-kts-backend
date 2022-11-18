b45a7bab8dfad52b5cb2dd4f77b61a94771b39a0
Zoran Bukorac <zoran00.zb@gmail.com>
2022-11-18 18:44:29 +0100

    Add unique constraint on both latitude and longitude and an index for latitude and longitude

--- src/main/java/nwt/kts/backend/entity/Point.java
+++ src/main/java/nwt/kts/backend/entity/Point.java
@@ -3,7 +3,10 @@ package nwt.kts.backend.entity;
 import javax.persistence.*;
 
 @Entity
-@Table(name="points")
+@Table(name="points",
+        uniqueConstraints = {
+        @UniqueConstraint(name = "UniqueLatitudeAndLongitude", columnNames = {"latitude", "longitude"})},
+        indexes = { @Index(name = "points_latitude_longitude_idx", columnList = "latitude, longitude")})
 public class Point {
 
     @Id
@@ -11,10 +14,10 @@ public class Point {
     @Column(name = "id", unique = true, nullable = false)
     private Integer id;
 
-    @Column(name = "latitude", unique = true)
+    @Column(name = "latitude")
     private double latitude;
 
-    @Column(name = "longitude", unique = true)
+    @Column(name = "longitude")
     private double longitude;
 
     public Point() {
